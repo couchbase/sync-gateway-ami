3360e1a5e218fc009db1bb5cc8f6d19c5dc619e8
Traun Leyden <tleyden@couchbase.com>
2017-03-11 09:02:58 -0800

    New amis

--- src/cloudformation_template.py
+++ src/cloudformation_template.py
@@ -232,13 +232,13 @@ def main():
 
     # Generated via http://uberjenkins.sc.couchbase.com/view/Build/job/couchbase-server-ami/
     couchbase_ami_ids_per_region = {
-        "us-east-1": "ami-907dda86",
+        "us-east-1": "ami-dad67bcc",
         "us-west-1": "ami-d45c05b4"
     }
 
     # Generated via http://uberjenkins.sc.couchbase.com/view/Build/job/sync-gateway-ami/
     sync_gateway_ami_ids_per_region = {
-        "us-east-1": "ami-ff9842e9",
+        "us-east-1": "ami-f1d77ae7",
         "us-west-1": "ami-4cf0ae2c"
     }
 
